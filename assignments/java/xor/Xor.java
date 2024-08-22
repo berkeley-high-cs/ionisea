72b2f36cb1796bf7fa0a9d226fd4169af63dc68b
ion <111394893+ionisea@users.noreply.github.com>
2024-08-22 14:38:31 -0700

    Updating assignments/java/xor/Xor.java

--- assignments/java/xor/Xor.java
+++ assignments/java/xor/Xor.java
@@ -21,7 +21,7 @@ public class Xor {
     String deciph = "";
     for (int i = 0; i<cipherBytes.length; i++) {
       cipherBytes[i] = (byte) Integer.parseInt(s.substring(2*i, 2*(i+1)),16);
-      System.out.print(cipherBytes[i]);
+      System.out.print(cipherBytes[i] + " ");
     }
     return "f";
   }
